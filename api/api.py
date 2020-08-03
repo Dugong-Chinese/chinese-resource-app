f4e9b3b6e506c1e184b0069b619a059924166965
Simon Ilincev <trilogicworlds@gmail.com>
2020-08-03 09:03:04 +0200

    cancel PR, fails for home screen

--- api/api.py
+++ api/api.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
-#cors = CORS(app, resources={r"/api/*": {"origins": ["http://localhost:3000", "https://test-mandarin-web-app.herokuapp.com"]}})
+cors = CORS(app, resources={r"/api/*": {"origins": ["http://localhost:3000", "https://test-mandarin-web-app.herokuapp.com"]}})
 
 # GET request, no parameters
 class SimpleExample(Resource):
@@ -37,6 +37,6 @@ api.add_resource(
     ComplexExample, "/api/multiply/<int:num>"
 )  # specify variable type (or typecast)
 
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+#     app.run()
 # you can start the server by cding to the directory and running python3 api.py; it will start on localhost:5000 (if not in use)
