54b9a3b72b4cd43e7b2a5ea54549de2a59771049
Destaq <trilogicworlds@gmail.com>
2020-08-03 09:36:40 +0200

    Revert "Merge branch 'master' into master"
    This reverts commit 30035ba9a731cc35792e52833d532a0e954d9ed9, reversing
    changes made to 57dca6471f49473073f47617d67d3d7df13750e4.

--- api/api.py
+++ api/api.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 app = Flask(__name__, static_folder="../build", static_url_path="/")
 api = Api(app)
-cors = CORS(app, resources={r"/api/*": {"origins": ["http://localhost:3000", "https://test-mandarin-web-app.herokuapp.com"]}})
+cors = CORS(app, resources={r"/api/*": {"origins": ["http://localhost:3000", "https://mandarin-web-app.herokuapp.com"]}})
 
 # GET request, no parameters
 class SimpleExample(Resource):
@@ -20,6 +20,7 @@ class SimpleExample(Resource):
         # get POST data with request.get_json()
         some_json = request.get_json()  # whatever was posted
         return {"you sent": some_json}
+
 
 class ComplexExample(Resource):
     """
@@ -37,6 +38,4 @@ api.add_resource(
     ComplexExample, "/api/multiply/<int:num>"
 )  # specify variable type (or typecast)
 
-# if __name__ == "__main__":
-#     app.run()
 # you can start the server by cding to the directory and running python3 api.py; it will start on localhost:5000 (if not in use)
