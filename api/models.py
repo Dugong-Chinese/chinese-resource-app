1269e65c41bdde2a28dd41e9b39f3268c9812f9a
Federico Salerno <itashadd+gitlab@gmail.com>
2020-08-06 23:34:29 +0200

    users table

--- api/models.py
+++ api/models.py
@@ -1,10 +1,19 @@
 """Database models for the RESTful API."""
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 
 db = SQLAlchemy()
 
 
-class User(db.Model):
-    pass
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_joined = db.Column(db.DateTime(timezone=True), server_default=text("NOW()"))
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    salt = db.Column(db.String, nullable=False)
+    # TODO known vocabulary
+
+    def __repr__(self):
+        return f"<Users {self.email}>"
